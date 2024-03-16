2980546f2b144561a2f3fb0ff2c3d350e768bee9
lmolinamar <molina4@un.org>
2024-03-16 08:41:45 +0100

    working simple langchain rag

--- langchainrag.py
+++ langchainrag.py
@@ -10,9 +10,30 @@ from glob import glob
 files = glob("*")
 
 from langchain_community.document_loaders import DirectoryLoader
-loader = DirectoryLoader('/users/luis/Documents/obsvault/', glob="**/*.md")
+loader = DirectoryLoader('/users/luis/Documents/obsvault/1on1', glob="**/*.md")
 docs = loader.load()
-print (len(docs))
+
+
+import logging
+
+# Create logger 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create console handler
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.DEBUG)
+
+# Create formatter and add to console handler
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+
+# Add console handler to logger
+logger.addHandler(c_handler)
+
+# Log message
+logger.info("Number of documents: %s", len(docs)) 
+
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
@@ -23,21 +44,27 @@ texts = text_splitter.split_documents(docs)
 embeddings = OpenAIEmbeddings()
 db = FAISS.from_documents(texts, embeddings)
 
-question = " what is the latest 1on1 with david?"
+# let's try to search some documents
+question = " what is the latest 1on1 with david donnellan, only include documents that have in the text the name of David?"
 searchDocs = db.similarity_search(question)
-print(searchDocs[0].page_content)
+#logger.info("Title of first document: : %s", searchDocs[0].page_content) 
+#print( "Title of first document: " + searchDocs[0].page_content)
 
 retriever = db.as_retriever(search_kwargs={"k": 3})
 
 from langchain.prompts import PromptTemplate
 
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible. 
+template = """Use the following pieces of context to answer the question at the end. 
+Keep the answer as concise as possible. 
 {context}
 Question: {question}
 Helpful Answer:"""
-QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
-from langchain import OpenAI
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+#logger.info(QA_CHAIN_PROMPT)
+
+
+from langchain_openai import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.agents import Tool
 
@@ -45,11 +72,19 @@ openai_llm = OpenAI(
     openai_api_key=OPENAI_API_KEY,
     temperature=0
 )
-qa_chain = RetrievalQA.from_chain_type(   
-  llm=openai_llm,   
-  chain_type="stuff",   
-  retriever=db.as_retriever(),   
-  chain_type_kwargs={"prompt": QA_CHAIN_PROMPT} 
-) 
-result = qa_chain ({ "query" : question }) 
+
+
+
+# Run the RetrievalQA chain with prompt
+qa_chain = RetrievalQA.from_chain_type(
+    openai_llm,
+    retriever=db.as_retriever(search_kwargs={"k": 3}),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+
+question = "when was the latest meeting with david donnellan?"
+
+result = qa_chain.invoke({"query": question})
 print(result["result"])
+print(result["source_documents"][0])
